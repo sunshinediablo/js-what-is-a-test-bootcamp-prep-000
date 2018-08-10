59376ae3616655fef82cbdaa4f611531e6fde093
Valerie Vallarta <valerievallarta@gmail.com>
2018-08-10 00:57:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+<<<<<<< HEAD
 console.log(message)
+=======
+consolde.log(message)
+>>>>>>> fece89e8d18328f8ca08f2202093a9a66f9c3931
